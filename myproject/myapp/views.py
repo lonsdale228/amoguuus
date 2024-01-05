cf449357e77f4d2629ee60af4a257508d745e0e2
bebus <v.gerashchenkoua@gmail.com>
2024-01-05 04:07:03 +0200

    fixed main page

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -4,3 +4,4 @@ from .models import Item
 def item_list(request):
     items = Item.objects.all()
     return render(request, 'myapp/item_list.html', {'items': items})
+
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('items/', include('myapp.urls')),
+    path('', include('myapp.urls')),
 ]
